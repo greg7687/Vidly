a314cca397f3bdaf448a032738b87527a43774b3
Greg <greg.owens87@gmail.com>
2017-07-17 00:13:55 -0400

    Synced up with Exercise 2 in Mosh GitHub

--- src/Vidly/Vidly/Controllers/MoviesController.cs
+++ src/Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,24 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        // GET: Movies/Index
+        public ViewResult Index()
+        {
+
+            var movies = GetMovies();
+
+            return View(movies);
+
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-E"}
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
 
-        }
-
-        [Route("movies/release/{year}/{moth:regex(\\{4}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
+            return View(viewModel);
         }
     }
 }

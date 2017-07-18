972450bdfbbe5cab9b0c3de7dacf691b6f439868
Greg <greg.owens87@gmail.com>
2017-07-17 21:21:15 -0400

    Updated Customer View.Index to display MembershipType.Name

---
+++ src/Vidly/Vidly/Migrations/201707180111213_PopulateMembershipTypeName.cs
@@ -0,0 +1,21 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypeName : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes Set Name = 'Pay as You Go' WHERE Id=1");
+            Sql("UPDATE MembershipTypes Set Name = 'Monthly' WHERE Id=2");
+            Sql("UPDATE MembershipTypes Set Name = 'Quarterly' WHERE Id=3");
+            Sql("UPDATE MembershipTypes Set Name = 'Annual' WHERE Id=4");
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
